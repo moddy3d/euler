9b42656209f267315a3fe5332d335c6a38b3f7f1
moddy3d <moddy3d@gmail.com>
2015-04-12 01:04:46 +0800

    Solution for problem 16

---
+++ p16/p16.hs
@@ -0,0 +1,13 @@
+-- Problem 16: Power digit sum
+--
+-- https://projecteuler.net/problem=16
+--
+-- 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+--
+-- What is the sum of the digits of the number 2^1000?
+
+import Data.Char (digitToInt)
+
+stringToSum s = foldl1 (+) $ map digitToInt s
+
+main = putStrLn $ show $ stringToSum $ show (2^1000) 
