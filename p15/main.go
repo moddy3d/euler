4f9b3c6318fcfa2080e9b9aeafe645aed0f2a5d1
moddyz <moddy3d@gmail.com>
2016-08-31 20:24:29 +1200

    Added recursive solution for p15

--- p15/main.go
+++ p15/main.go
@@ -2,16 +2,39 @@ package main
 
 import "fmt"
 
-var (
-	n = 40
-	k = 20
-)
+func multiplicativeBinomialCoeff() {
+	var (
+		n     = 40
+		k     = 20
+		total = 1
+	)
 
-func main() {
-	total := 1
 	for i := 0; i < k; i++ {
 		total *= n - i
 		total /= i + 1
 	}
 	fmt.Println(total)
+}
+
+func recurse(x, y int, cache *[20][20]int) int {
+	if x == 0 || y == 0 {
+		return 1
+	}
+
+	if cache[x-1][y-1] != 0 {
+		return cache[x-1][y-1]
+	} else {
+		cache[x-1][y-1] = recurse(x, y-1, cache) + recurse(x-1, y, cache)
+		return cache[x-1][y-1]
+	}
+}
+
+func recursiveWithCache() {
+	cache := &[20][20]int{}
+	fmt.Println(recurse(20, 20, cache))
+}
+
+func main() {
+	multiplicativeBinomialCoeff()
+	recursiveWithCache()
 }
